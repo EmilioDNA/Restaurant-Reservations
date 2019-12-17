ca47064b24f3fd1d4b6f548c1f29ecc107903661
Emilio Najera <je.dominguez@ebc.edu.mx>
2019-12-16 23:48:20 -0600

    Included Insert and Retrieve functions for the Restaurant Model

--- app.py
+++ app.py
@@ -3,14 +3,86 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+from models import setup_db, Restaurant, DinningTable, Reservation
+
+
+RESTAURANTS_PER_PAGE = 10
+# Pagination method
+def paginate_restaurants(request, selection):
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * RESTAURANTS_PER_PAGE
+    end = start + RESTAURANTS_PER_PAGE
+
+    restaurants = [restaurant.format() for restaurant in selection]
+    current_restaurants = restaurants[start:end]
+    return current_restaurants
+
+
+
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
-    
-  return app
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
+
+    @app.route('/')
+    def hello():
+      return 'Hello'
+
+    @app.route('/restaurants', methods=['GET'])
+    def retrieve_restaurants():
+      selection = Restaurant.query.order_by(Restaurant.id).all()
+      current_restaurants = paginate_restaurants(request, selection)
+
+      if len(current_restaurants) == 0:
+        abort(404)
+
+      return jsonify({
+        'success': True,
+        'restaurants': current_restaurants,
+        'total_restaurants':  len(selection)
+      })
+
+    @app.route('/restaurants', methods=['POST'])
+    def create_restaurant():
+        body = request.get_json()
+
+        new_title = body.get('title', None)
+        new_city = body.get('city', None)
+        new_state = body.get('state', None)
+        new_address = body.get('address', None)
+        new_phone = body.get('phone', None)
+        new_image_link = body.get('image_link', None)
+
+        try:
+
+            restaurant = Restaurant(title=new_title,
+                                    city=new_city,
+                                    state=new_state,
+                                    address=new_address,
+                                    phone=new_phone,
+                                    image_link=new_image_link)
+            restaurant.insert()
+
+            selection = Restaurant.query.order_by(Restaurant.id).all()
+            current_restaurants = paginate_restaurants(request, selection)
+
+            return jsonify({
+                'success': True,
+                'created': restaurant.id,
+                'restaurants': current_restaurants,
+                'total_restaurants': len(selection)
+            })
+
+        except: 
+            abort(422)
+
+
+
+
+    return app
 
 APP = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    APP.run(host='127.0.0.1', port=5000, debug=True)
--- models.py
+++ models.py
@@ -1,10 +1,9 @@
 import os
-from sqlalchemy import Table, Column, String, Integer, ForeignKey, Datetime, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_name= "reservations"
-database_path = "postges://{}@{}/{}".format('postgres:postgres', 'localhost:5433', database_name)
+database_path = "postgres://{}@{}/{}".format('postgres:postgres', 'localhost:5433', database_name)
 
 
 db = SQLAlchemy()
@@ -38,14 +37,37 @@ def setup_db(app, database_path=database_path):
 class Restaurant(db.Model):
     __tablename__ = 'restaurant'
 
-    id = Column(Integer, primary_key=True)
-    title = Column(String)
-    city = Column(String)
-    state = Column(String)
-    address = Column(String)
-    phone = Column(String)
-    image_link = Column(String)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    city = db.Column(db.String)
+    state = db.Column(db.String)
+    address = db.Column(db.String)
+    phone = db.Column(db.String)
+    image_link = db.Column(db.String)
     tables = db.relationship('DinningTable', backref='restaurant', lazy=True)
+
+    def __init__(self, title, city, state, address, phone, image_link):
+        self.title = title
+        self.city = city
+        self.state = state 
+        self.address = address
+        self.phone = phone
+        self.image_link = image_link
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def format(self):
+        return {
+            'id': self.id,
+            'city': self.city,
+            'state': self.state,
+            'address': self.address,
+            'phone': self.phone,
+            'image_link': self.image_link,
+        }
+
 
 '''
     This is the the table that connects the Reservation with the DinningTable 
@@ -54,11 +76,10 @@ class Restaurant(db.Model):
     One DinningTable may be in multiple Reservations (as long as the tables are available, which relies on the start and end time of the reservation).
     This limitations will be implemented in the controller to avoid that a user reserves a table in the same period of time.
 '''
-reservation_tables = Table('reservation_tables',
-    Column('reservation_id', Integer, ForeignKey('reservation.id'), primary_key=True),
-    Column('dinning_table_id', Integer, ForeignKey('dinning_table.id'), primary_key=True)
+reservation_tables = db.Table('reservation_tables',
+    db.Column('reservation_id', db.Integer, db.ForeignKey('reservation.id'), primary_key=True),
+    db.Column('dinning_table_id', db.Integer, db.ForeignKey('dinning_table.id'), primary_key=True)
     )
-
 
 '''
     This is the Reservation model that is related in a Many to Many relationship to the DinningTable model
@@ -66,9 +87,9 @@ reservation_tables = Table('reservation_tables',
 class Reservation(db.Model):
     __tablename__ = 'reservation'
 
-    id = Column(Integer, primary_key=True)
-    start_time = Column(Datetime())
-    end_time = Column(Datetime())
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime())
+    end_time = db.Column(db.DateTime())
     dinning_tables = db.relationship('DinningTable', secondary=reservation_tables,
                         backref=db.backref('order', lazy=True))
 
@@ -79,12 +100,8 @@ class Reservation(db.Model):
 class DinningTable(db.Model):
     __tablename__ = 'dinning_table'
 
-    id = Column(Integer, primary_key=True)
-    code = Column(String)
-    capacity = Column(Integer)
-    restaurant_id = Column(Integer, ForeignKey('restaurant.id'), nullable=False)
-
-
-
-
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String)
+    capacity = db.Column(db.Integer)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
 
